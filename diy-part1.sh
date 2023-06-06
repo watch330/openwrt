63a0ca63ca5089f4cb707e687ca8c4aa59712999
katharsis <34093544+watch330@users.noreply.github.com>
2023-06-06 17:18:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,5 +18,5 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # custom packages
-src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
